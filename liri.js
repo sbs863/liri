2d4c1ce289e6d2fdecba91cacdbc932ba68762fa
Sam Smith <samtrey@gmail.com>
2016-10-08 14:46:11 -0500

    streamlined

--- liri.js
+++ liri.js
@@ -1,13 +1,28 @@
 var inquirer = require('inquirer');
+var fs = require('fs');
 var request = require('request');
 var creds = require('./keys.js');
 var Twitter = require('twitter');
 var spotify = require('spotify');
 var client = new Twitter(creds.twitterKeys);
+var readMyData = ('./random.txt');
+
+random = [];
 
 
-var song = "";
-var movie = "";
+var options = {
+    'Search a song with Spotify': {
+        message: 'Please enter the song you wish to find',
+        fun: spot,
+        callback: printSongs
+    },
+    'Find a movie with OMDB': {
+        message: 'Please enter the movie you wish to find',
+        fun: omdb,
+        callback: printMovies
+    }
+};
+
 var liri = function() {
 
 
@@ -24,7 +39,9 @@ var liri = function() {
                 name: 'Search a song with Spotify'
             }, {
                 name: 'Find a movie with OMDB'
-            },
+            }, {
+                name: 'Do what it says'
+            }
         ],
         validate: function(answer) {
             if (answer.length > 1) {
@@ -33,111 +50,155 @@ var liri = function() {
             return true;
         }
     }]).then(function(answers) {
-        console.log(answers.options[0]);
 
         if (answers.options[0] == 'Display my last twenty tweets') {
-            twitter();
+            twitter(printTweets);
 
+        } else {
+            var selected = options[answers.options[0]];
+
+            inquirer.prompt({
+                    name: 'test',
+                    message: selected.message,
+                    type: 'input'
+                })
+                .then(function(search) {
+                    
+                    selected.fun(search.test, selected.callback);
+                });
         }
 
 
-        if (answers.options[0] === 'Search a song with Spotify') {
-            inquirer.prompt([
+        // if (answers.options[0] === 'Search a song with Spotify') {
+        //     inquirer.prompt([
 
-                {
-                    type: 'input',
-                    message: 'Please enter the song you wish to find',
-                    name: 'song',
+        //         {
+        //             type: 'input',
+        //             message: 'Please enter the song you wish to find',
+        //             name: 'song',
 
+        //         }
+
+        //     ]).then(function(search) {
+
+        //         song = search.song;
+        //         spot();
+
+        //     });
+        // }
+
+        // if (answers.options[0] === 'Find a movie with OMDB') {
+        //     inquirer.prompt([
+
+        //         {
+        //             type: 'input',
+        //             message: 'Please enter the movie you wish to find',
+        //             name: 'movie',
+
+        //         }
+
+        //     ]).then(function(search) {
+
+        //         movie = search.movie;
+        //         omdb();
+
+        //     });
+        // }
+        if (answers.options[0] === 'Do what it says') {
+            fs.readFile('random.txt', 'utf8', function(err, data) {
+                if (err) {
+                    return console.log(err);
                 }
-
-            ]).then(function(search) {
-
-                song = search.song;
-                console.log(song);
-                spot();
-
-            });
-        }
-
-        if (answers.options[0] === 'Find a movie with OMDB') {
-            inquirer.prompt([
-
-                {
-                    type: 'input',
-                    message: 'Please enter the movie you wish to find',
-                    name: 'movie',
-
-                }
-
-            ]).then(function(search) {
-
-                movie = search.movie;
-                console.log(movie);
-                omdb();
-
+                random.push(data);
+                // console.log(random);
+                song = data[1];
             });
         }
     });
 };
 liri();
 
-function twitter() {
+function twitter(callback) {
     var params = { user_id: '253278398', count: 21, trim_user: true, };
     client.get('statuses/user_timeline', params, function(error, tweets, response) {
         if (!error) {
-            for (var i = 0; i <= 20; i++) {
-                console.log(JSON.stringify(tweets[i].created_at, null, 2));
-                console.log(JSON.stringify(tweets[i].text, null, 2));
-                console.log('------------------------------------');
-            }
-
-        } else {
-            console.log(error);
-
-
+            callback(null, tweets);
+            return liri();
         }
+        callback(error);
     });
 
 }
 
-function spot() {
-    if (song === ""); {
+function printTweets(error, tweets) {
+    if (error) {
+        console.log(error);
+    } else {
+        for (var i = 0; i <= 20; i++) {
+            console.log(JSON.stringify(tweets[i].created_at, null, 2));
+            console.log(JSON.stringify(tweets[i].text, null, 2));
+            console.log('------------------------------------');
+        }
+    }
+}
+
+function spot(song, callback) {
+    if (song === "") {
         song = "The Sign";
     }
-    request('https://api.spotify.com/v1/search?q=' + song + '&type=track&market=US&limit=10', function(error, response, data) {
+    console.log(song);
+    request('https://api.spotify.com/v1/search?q=' + song + '&type=track&market=US&limit=10', function(error, response, song) {
         if (!error) {
-            for (var i = 0; i < 10; i++) {
-
-
-                console.log("Artist(s): " + JSON.parse(data).tracks.items[i].artists[0].name);
-                console.log("Album: " + JSON.parse(data).tracks.items[i].album.name);
-                console.log("Song: " + JSON.parse(data).tracks.items[i].name);
-                console.log("Preview: " + JSON.parse(data).tracks.items[i].preview_url);
-                console.log("-----------------------------");
-            }
+            callback(null, song);
+            return liri();
         }
+    });
+}
+
+function printSongs(error, song) {
+    if (error) {
+        console.log(error);
+    } else {
+        for (var i = 0; i < 10; i++) {
+            console.log("Artist(s): " + JSON.parse(song).tracks.items[i].artists[0].name);
+            console.log("Album: " + JSON.parse(song).tracks.items[i].album.name);
+            console.log("Song: " + JSON.parse(song).tracks.items[i].name);
+            console.log("Preview: " + JSON.parse(song).tracks.items[i].preview_url);
+            console.log("-----------------------------");
+        }
+    }
+}
+
+function omdb(movie, callback) {
+    if (movie === "") {
+        movie = "Mr. Nobody";
+    }
+    request('http://www.omdbapi.com/?t=' + movie + '&y&type=movie&plot=short&r=json&tomatoes=true', function(error, response, movie) {
+        if (!error) {
+            callback(null, movie);
+            return liri();
+
+        }
+
     });
 
 }
 
-function omdb() {
-        if (movie === ""); {
-        movie = "Mr. Nobody";
-    }
-    request('http://www.omdbapi.com/?t=' + movie + '&y&type=movie&plot=short&r=json&tomatoes=true', function(error, response, body) {
+function printMovies(error, movie) {
+    if (error) {
+        console.log(error);
+    } else {
 
-        if (!error) {
-            console.log("Title: " + JSON.parse(body).Title);
-            console.log("Released: " + JSON.parse(body).Released);
-            console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
-            console.log("Plot: " + JSON.parse(body).Plot);
-            console.log("Actors: " + JSON.parse(body).Actors);
-            console.log("Rottem Tomatoes Rating: " + JSON.parse(body).tomatoRating);
-            console.log("Language: " + JSON.parse(body).Language);
-            console.log("Produced in: " + JSON.parse(body).Country);
-            console.log("Rotten Tomatoes URL: " + JSON.parse(body).tomatoURL);
-        }
-    });
+        console.log("Title: " + JSON.parse(movie).Title);
+        console.log("Released: " + JSON.parse(movie).Released);
+        console.log("IMDB Rating: " + JSON.parse(movie).imdbRating);
+        console.log("Plot: " + JSON.parse(movie).Plot);
+        console.log("Actors: " + JSON.parse(movie).Actors);
+        console.log("Rottem Tomatoes Rating: " + JSON.parse(movie).tomatoRating);
+        console.log("Language: " + JSON.parse(movie).Language);
+        console.log("Produced in: " + JSON.parse(movie).Country);
+        console.log("Rotten Tomatoes URL: " + JSON.parse(movie).tomatoURL);
+
+    }
 
 }
